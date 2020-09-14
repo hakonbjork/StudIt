4a4269db0979d33ad80f7baf744704845456a6c8
Håkon Flugsrud Bjørkum <haakofb@stud.ntnu.no>
2020-09-14 21:04:21 +0000

    Made the login-button load a new fxml-file

--- StudIt/src/main/java/studit/ui/LoginController.java
+++ StudIt/src/main/java/studit/ui/LoginController.java
@@ -1,29 +1,40 @@
 package studit.ui;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
-public class LoginController{
+public class LoginController {
 
-    @FXML  PasswordField passwordField;
-    @FXML TextField usernameField;
-    @FXML Button loginButton;
+    @FXML
+    PasswordField passwordField;
+    @FXML
+    TextField usernameField;
+    @FXML
+    Button loginButton;
+    @FXML VBox vBox;
 
-    public LoginController(){
+    public LoginController() {
     }
 
-    public void initialize(){
+    public void initialize() {
         studit.core.LoginManager.initialize();
     }
 
-    ///Checks if login creditentials are correct, and does an action corresponding
-    public void loginButtonAction(){
+    /// Checks if login creditentials are correct, and does an action corresponding
+    public void loginButtonAction() throws IOException {
         String username = usernameField.getText();
         String password = passwordField.getText();
         if (studit.core.LoginManager.match(username, password)){
             System.out.print("Success ");
+            AnchorPane pane = FXMLLoader.load(getClass().getResource("App.fxml"));
+            vBox.getChildren().setAll(pane);
         }
         else{
             System.out.print("Failure, "+username+ ", "+password + " ");
