1089e838001816248f662d211e960c6418423389
Anita Stuberg Guldseth <anitasg@stud.ntnu.no>
2020-09-22 12:08:18 +0000

    made a courseController

---
+++ StudIt/src/main/java/studit/ui/Course.java
@@ -0,0 +1,27 @@
+package studit.ui;
+	
+import javafx.application.Application;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Course extends Application {
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("Course.fxml"));
+        Scene scene = new Scene(root);
+        scene.getStylesheets().add(getClass().getResource("mainPage_style.css").toExternalForm());
+		
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("StudIt");
+		primaryStage.show();
+	}
+	
+}
--- StudIt/src/main/java/studit/ui/CourseController.java
+++ StudIt/src/main/java/studit/ui/CourseController.java
@@ -1,16 +1,67 @@
 package studit.ui;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import studit.core.chatbot.Chatbot;
+import javafx.scene.control.Button;
 
-
-public class CourseController{ 
+public class CourseController {
+    
+    public static Chatbot chatbot = null;
+    private String comment;
 
     @FXML
     private Label label;
 
+    @FXML
+    private Button mainpage_btn;
+
+    @FXML
+    private Button chatbot_btn;
+
+    @FXML
+    private Button ntnu_btn;
+
+    @FXML
+    private Button logout_btn;
+
+    @FXML
+    private TextArea commentField;
+
+    @FXML
+    void logoutAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void mainpageAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void ntnuAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void openChatBot(ActionEvent event) {
+         if (chatbot == null) {
+    		chatbot = new Chatbot();
+    	} else {
+    		chatbot.show();
+    	}
+    }
+
     public void setLabelText(String text){
        label.setText(text);
    }
+
+   public void writeComment(String text){
+        comment =  commentField.getText();
+        commentField.setText(comment);
+   }
+
 
 }
