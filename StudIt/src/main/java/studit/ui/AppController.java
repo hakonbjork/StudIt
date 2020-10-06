d58bf840aef966ca39706d2b212171a8e6e6cc51
Anita Stuberg Guldseth <anitasg@stud.ntnu.no>
2020-10-06 09:31:04 +0000

    changes in both controllers (scene fixes)

--- StudIt/src/main/java/studit/ui/AppController.java
+++ StudIt/src/main/java/studit/ui/AppController.java
@@ -1,168 +1,132 @@
 package studit.ui;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
-import java.nio.charset.StandardCharsets;
-import java.util.Map;
+
+
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
 import javafx.event.EventHandler;
-import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import studit.core.chatbot.Chatbot;
-import studit.core.mainpage.CourseItem;
-import studit.core.mainpage.CourseList;
-import studit.json.CoursePersistence;
 
-public class AppController {
+public class AppController implements ChangeListener<String> { 
 
-    @FXML
-    private ListView<String> coursesList;
-    @FXML
-    private Button mainPageAction;
-    @FXML
-    private Button openChatBot;
-    @FXML
-    private Button ntnuAction;
-    @FXML
-    private Button logoutAction;
-    @FXML
-    private BorderPane rootPane;
-    @FXML
-    private AnchorPane mainPane;
+    @FXML private ListView<String> coursesList;
+    @FXML private Button mainPageAction;
+    @FXML private Button openChatBot;
+    @FXML private Button ntnuAction;
+    @FXML private Button logoutAction;
+    @FXML private BorderPane rootPane;
+    @FXML private AnchorPane mainPane;
+    @FXML private TextField searchField;
 
-    @FXML
-    private Button mainPage_btn;
-    @FXML
-    private Button chatbot_btn;
-    @FXML
-    private Button ntnu_btn;
-    @FXML
-    private Button logout_btn;
+    @FXML private Button mainPage_btn;
+    @FXML private Button chatbot_btn;
+    @FXML private Button ntnu_btn;
+    @FXML private Button logout_btn;
+
 
     public static Chatbot chatbot = null;
-    private CourseList courseList = new CourseList();
-    private CoursePersistence coursePersistence = new CoursePersistence();
+    public App app;
     public Scene mainScene;
-
-    // makes class more testable
-    CourseList getCourseList() {
-        return this.courseList;
-    }
-
-    ObservableList<String> list = FXCollections.observableArrayList();
-
-    private void initializeCourseList() {
-        // setter opp data
-        Reader reader = null;
-        // try to read file from home folder first
-
-        try {
-            reader = new FileReader("/main/resources/studit/db/db.json");
-        } catch (FileNotFoundException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-
-        try {
-
-            courseList = coursePersistence.readCourseList(reader);
-
-            for (CourseItem item : courseList.getCourseItems()) {
-
-                list.add(item.getFagkode());
-
-            }
-
-            coursesList.setItems(list);
-
-        } catch (IOException e) {
-
-            System.out.println(e);
-
-        } finally {
-
-            try {
-                if (reader != null) {
-                    reader.close();
-                }
-            } catch (IOException e) {
-                // ignore
-            }
-        }
-    }
-
-    @FXML
-    public void initialize() {
-
-        initializeCourseList();
-
-        updateData();
-
-        coursesList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
-
-        // Actions on clicked list item
-        mouseClicked();
-    }
-
-    /**
-     * Gives the user a choice to open NTNU homepage in web-browser
-     * 
-     * @return none
-     */
-    @FXML
-    void openChatBot(ActionEvent event) {
-        if (chatbot == null) {
-            chatbot = new Chatbot();
-        } else {
-            chatbot.show();
-        }
-    }
-
-    public static void closeChatbot() {
-        chatbot = null;
-    }
+    private ObservableList<String> list = FXCollections.observableArrayList();
+    private String label;
+    
 
     public void setSecondScene(Scene scene) {
         mainScene = scene;
     }
 
+
+    public String getLabel(){
+        return this.label;
+    }
+
+    public void setLabel(String label){
+        this.label = label;
+    }
     /**
-     * Function to search for subjects. The listview will then only show subjects
-     * with the letters in the search field
-     * 
-     * @return none
-     */
-    @FXML
-    void handleSearchAction(KeyEvent event) {
-
+    * Function to initialize AppController
+    * @return none
+    */
+    public void initialize(){
+        loadData();
+        coursesList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
+        //Actions on clicked list item
+        mouseClicked();
+            // searchField.textProperty().addListener(new ChangeListener<String>.changed(ObservableValue<? extends String>, String, String) {
+            //     @Override
+            //     public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+            //         filterCoursesList((String) oldValue, (String) newValue);
+            //     }
+            // });
     }
 
-    public void changed(ObservableValue observable, Object oldVal, Object newVal) {
-        search((String) oldVal, (String) newVal);
-    }
-
-    public void search(String oldVal, String newVal) {
-        if (oldVal != null && (newVal.length() < oldVal.length())) {
+    public void filterCoursesList(String oldValue, String newValue) {
+    
+        ObservableList<String> filteredList = FXCollections.observableArrayList();
+        if(searchField == null || (newValue.length() < oldValue.length()) || newValue == null) {
             coursesList.setItems(list);
         }
+        else {
+            newValue = newValue.toUpperCase();
+            for(String course : coursesList.getItems()) {
+                if(course.toUpperCase().contains(newValue)) {
+                    filteredList.add(course);
+                }
+            }
+            coursesList.setItems(filteredList);
+        }    
+    }
+
+    
+    /**
+    * Opens chatbot
+    * @return none
+    */
+    @FXML
+    void openChatBot(ActionEvent event) {
+        if (chatbot == null) {
+    		chatbot = new Chatbot();
+    	} else {
+    		chatbot.show();
+    	}
+    }
+    /**
+    * closes chatbot 
+    * @return none
+    */
+    public static void closeChatbot() {
+        chatbot = null;
+    }
+
+
+
+    /**
+    * Function to search for subjects. The listview will then only show subjects with 
+    * the letters in the search field
+    * @return none
+    */
+    @FXML
+    public void handleSearchViewAction(String oldVal, String newVal) {
+        if (oldVal != null && (newVal.length() < oldVal.length())) {
+        coursesList.setItems(list);
+    }
 
         String value = newVal.toUpperCase();
 
@@ -170,129 +134,109 @@ public class AppController {
         for (Object entry : coursesList.getItems()) {
             boolean match = true;
             String entryText = (String) entry;
-            if (!entryText.toUpperCase().contains(value)) {
-                match = false;
-                break;
-            }
-            if (match) {
-                subentries.add(entryText);
-            }
+        if (!entryText.toUpperCase().contains(value)) {
+            match = false;
+            break;
+        } if (match) {
+            subentries.add(entryText);
         }
+    }
         coursesList.setItems(subentries);
     }
 
     /**
-     * Should give the option to go to the subjects web-page
-     * 
-     * @return none
-     */
+    * Should give the option to go to the subjects web-page
+    * @return none
+    */
     @FXML
     void handleNtnuAction(ActionEvent event) {
 
-        // go to NTNU homepage (question if you want to open web-browser)?
-        // or a new window with information about NTNU?
+        //go to NTNU homepage (question if you want to open web-browser)?
+        //or a new window with information about NTNU?
 
     }
 
-    @FXML
-    void searchView(ActionEvent event) {
-
-    }
-
-    /**
+     /** 
      * logs user out, and opens to login scene, closes current scene
      */
-    @FXML
-    void handleLogoutAction(ActionEvent event) {
-        try {
-
+     @FXML void handleLogoutAction(ActionEvent event) {
+         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
             Parent root = loader.load();
-
+    
             Stage stage2 = new Stage();
             stage2.setScene(new Scene(root));
             stage2.setTitle("StudIt");
             stage2.show();
 
-            Stage stage = (Stage) logout_btn.getScene().getWindow();
+            Stage stage = (Stage) rootPane.getScene().getWindow();
             stage.hide();
+            
 
         } catch (IOException e) {
-            System.out.println(e);
-        }
-    }
+                System.out.println(e);
+            }
+	    }
 
-    /**
-     * redirects user to the main page
-     * 
-     * @return none
-     */
+
+    /** 
+    * redirects user to the main page
+    * @return none
+    */
     @FXML
     void handleMainPageAction() {
-        // nothing should really happen when you are in the home page other than maybe
-        // refresh(?)
+        //nothing should really happen when you are in the home page other than maybe refresh(?)
     }
 
-    /**
-     * A function that does something when a element in the listview is clicked on.
-     * 
-     * @return None
-     */
-    public void mouseClicked() {
-        // Detecting mouse clicked
-        coursesList.setOnMouseClicked(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent arg0) {
-                String name = coursesList.getSelectionModel().getSelectedItem();
-                System.out.println(name);
 
+    
+    /** A function that does something when a element in the listview is clicked on.
+    * @return None
+    */
+    public void mouseClicked(){
+		//Detecting mouse clicked
+		coursesList.setOnMouseClicked(new EventHandler<MouseEvent>(){
+            private String label;
+			@Override
+			public void handle(MouseEvent arg0) {
+                //super.label = coursesList.getSelectionModel().getSelectedItem();	
+                
                 try {
-                    Stage primaryStage = (Stage) ((Node) arg0.getSource()).getScene().getWindow();
-                    primaryStage.setScene(mainScene);
-
                     // FXMLLoader loader = new FXMLLoader(getClass().getResource("Course.fxml"));
-                    // Parent root = loader.load();
+                    // CourseController courseController = (CourseController) loader.getController();
+                    // courseController.setLabel(label);
 
-                    // CourseController courseController = loader.getController();
-                    // courseController.setLabel(name);
-
-                    // Stage stage = new Stage();
-                    // stage.setScene(new Scene(root));
-                    // stage.show();
+                    Stage primaryStage = (Stage)((Node)arg0.getSource()).getScene().getWindow();
+                    primaryStage.setScene(mainScene);
                     
                 } catch (Exception e) {
-                    System.out.println(e);
+                        System.out.println(e);
                 }
             }
-
-        });
+        });            
+    }
+           
+                    
+            
+    /** This function should actually fetch data from a database. This will be implemented later.
+    * @return None
+    */
+    private void loadData() {
+        String a = "TDT4109";
+        String b =  "TMA4145";
+        String c = "TTM4175";
+        String d = "IT1901";
+        list.addAll(a,b,c,d);
+        coursesList.setItems(list);
     }
 
-    /**
-     * This function should actually fetch data from a database. This will be
-     * implemented later.
-     * 
-     * @return None
-     */
-    public void updateData() {
+    public ObservableList<String> getData(){
+        return (ObservableList<String>) list;
+    }
 
-        for (CourseItem item : courseList) {
-
-            if (!list.contains(item.getFagkode())) {
-                list.add(item.getFagkode());
-            }
-
-        }
-        coursesList.setItems(list);
-
-        try (Writer writer = new FileWriter("src/main/resources/studit/db/db.json", StandardCharsets.UTF_8)) {
-
-            coursePersistence.writeCourseList(courseList, writer);
-
-        } catch (IOException e) {
-
-            System.err.println("Fikk ikke skrevet til db.json");
-        }
+    @Override
+    public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+        // TODO Auto-generated method stub
 
     }
 }
--- StudIt/src/main/java/studit/ui/CourseController.java
+++ StudIt/src/main/java/studit/ui/CourseController.java
@@ -8,6 +8,15 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -24,34 +33,39 @@ public class CourseController implements Initializable {
     @FXML private BorderPane rootPane;
     @FXML private FlowPane flowPane;
 
-    @FXML private Button mainPageAction;
-    @FXML private Button chatbotAction;
-    @FXML private Button ntnuAction;
-    @FXML private Button logoutAction;
-    @FXML private Button addComment;
+    @FXML private Button chatbot_btn;
+    @FXML private Button ntnu_btn;
+    @FXML private Button logout_btn;
+    @FXML private Button addComment_btn;
     @FXML private Button mainPage_btn;
-    @FXML private Button openChatBot;
 
     @FXML private Label comments;
     @FXML private Label label;
+    @FXML private Label rating;
+
 
     @FXML private TextArea comment1;
     @FXML private TextArea comment2;
     @FXML private TextArea comment3;
-    @FXML private TextArea comment4;
     @FXML private TextArea commentField;
     @FXML private TextField search;
+    @FXML private TextArea courseText;
+
 
     App app = new App();
     Course course = new Course();
     public static Chatbot chatbot = null;
     private Scene mainScene;
-   
+
+    public void setMainScene(Scene scene) {
+        mainScene = scene;
+    }
 
 
 
    	@Override
     public void initialize(URL location, ResourceBundle resources) {
+        courseText.setEditable(false);
     }
 
    /** 
@@ -63,40 +77,30 @@ public class CourseController implements Initializable {
        this.label.setText(label);
    }
      
-
-
-   public void setMainScene(Scene scene) {
-        mainScene = scene;
-    }
-
-    
-
-
     /**
     * logs user out, and goes to login scene
     * closes the current window
     * @return none
      */
-    @FXML
-    void handleLogoutAction() {
-        try {
+     @FXML void handleLogoutAction(ActionEvent event) {
+         try {
             FXMLLoader loader = new FXMLLoader(getClass().getResource("Login.fxml"));
             Parent root = loader.load();
+    
+            Stage stage2 = new Stage();
+            stage2.setScene(new Scene(root));
+            stage2.setTitle("StudIt");
+            stage2.show();
 
-            Stage controllerStage = (Stage) mainPageAction.getScene().getWindow();
-            // do what you have to do
-            controllerStage.hide();
-        
-            Stage stage = new Stage();
-            stage.setScene(new Scene(root));
-            stage.show();
+            Stage stage = (Stage) rootPane.getScene().getWindow();
+            stage.hide();
             
-             
 
-            } catch (IOException e) {
+        } catch (IOException e) {
                 System.out.println(e);
             }
-	}
+	    }
+	
 
 
     /** 
@@ -104,23 +108,15 @@ public class CourseController implements Initializable {
     * @return none
     */
     @FXML
-    void handleMainPageAction(ActionEvent actionEvent ) {
-        Stage primaryStage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
-        primaryStage.setScene(mainScene);
-    }
-        // try {
-        //     FXMLLoader loader = new FXMLLoader(getClass().getResource("App.fxml"));
-        //     Parent root = loader.load();
-
-        //     Stage stage = new Stage();
-        //     stage.setScene(new Scene(root));
-        //     stage.show();
-
-        //     } catch (IOException e) {
-        //         System.out.println(e);
-        //     }
-		// }
-		
+    void handleMainPageAction(ActionEvent actionEvent) {
+        try {
+            Stage primaryStage = (Stage)((Button)actionEvent.getSource()).getScene().getWindow();
+            primaryStage.setScene(mainScene);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+            }
+        
 
     /**
     * Gives the user a choice to open NTNU homepage in web-browser
@@ -153,7 +149,7 @@ public class CourseController implements Initializable {
      void handleAddCommentAction(ActionEvent event) {
           String comment = commentField.getText();
 
-          if(comment1.getText().equals("") || (! comment1.getText().equals("")) && (! comment2.getText().equals("")) && (! comment3.getText().equals("")) || (! comment4.getText().equals(""))){
+          if(comment1.getText().equals("") || (! comment1.getText().equals("")) && (! comment2.getText().equals("")) && (! comment3.getText().equals(""))){
                 comment1.setText(comment);
           }
           else if((! comment1.getText().equals("")) && (comment2.getText().equals(""))){
@@ -162,9 +158,15 @@ public class CourseController implements Initializable {
           else if((! comment1.getText().equals("")) && (! comment2.getText().equals(""))){
               comment3.setText(comment);
           }
-          else if((! comment1.getText().equals("")) && (! comment2.getText().equals("") && (! comment3.getText().equals("")))){
-              comment4.setText(comment);
+          else if((! comment1.getText().equals("")) && (! comment2.getText().equals(""))){
+              comment3.setText(comment);
           }
+   }
+
+   @FXML 
+   void setRating(double rating){
+       String r = String.valueOf(rating);
+       this.rating.setText(r);
    }
 
    
