61687e1f186f9ddea82d36ca72d63e86623cd47e
Mithunan Sivakumar <mithunas@stud.ntnu.no>
2020-10-06 19:22:01 +0000

    fikset modularisering

---
+++ StudIt/fxui/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module studit {
+    requires transitive com.fasterxml.jackson.core;
+    requires transitive com.fasterxml.jackson.databind;
+
+    requires javafx.fxml;
+    requires transitive javafx.graphics;
+    requires javafx.controls;
+	requires com.fasterxml.jackson.annotation;
+	requires guava;
+
+    exports studit.ui;
+    exports studit.core;
+
+    opens studit.ui to javafx.fxml;
+    opens studit.core.chatbot to com.fasterxml.jackson.databind;
+}
