c430600762299ca1fdeea6ea5a797c964c46c81c
Håkon Flugsrud Bjørkum <haakofb@stud.ntnu.no>
2020-10-06 08:15:12 +0000

    Attempt to "manually merge" mainpage and login

---
+++ StudIt/src/main/java/studit/core/User.java
@@ -0,0 +1,58 @@
+package studit.core;
+
+public class User {
+
+    private String name;
+    private String username;
+    private String mail;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String name, String username, String mail, String password) {
+        this.name = name;
+        this.username = username;
+        this.mail = mail;
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "username='" + username + '\'' +
+                '}';
+    }
+}
