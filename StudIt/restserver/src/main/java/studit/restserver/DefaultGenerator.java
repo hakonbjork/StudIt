5bac286033b4d92a3df09f020a1225e995e4dba3
Erlend Hermanrud <erleherm@stud.ntnu.no>
2020-10-17 18:10:54 +0000

    Add default generator

--- StudIt/restserver/src/main/java/studit/restserver/DefaultGenerator.java
+++ StudIt/restserver/src/main/java/studit/restserver/DefaultGenerator.java
@@ -1,7 +1,22 @@
 package studit.restserver;
 
+import java.util.List;
+
 import studit.core.mainpage.CourseItem;
 
 public class DefaultGenerator {
-  CourseItem testItem = new CourseItem();
+
+  public void testSerializers() {
+      CourseItem testItem = new CourseItem();
+      testItem.setFagkode("TMA4140");
+      testItem.setFagnavn("bruh");
+      testItem.setInformasjon("Info");
+      testItem.setPensumlitteratur("bok");
+      testItem.setAnbefaltLitteratur("bok2");
+      testItem.setTips("Dette var lurt");
+      testItem.setEksamensdato("03/12/2002");
+      testItem.setVurderingsform("hjemmeEKsamen");
+      testItem.setHjelpemidler("alle");
+      testItem.setVurderinger(List.of(3, 5, 9, 8));
+  }
 }
