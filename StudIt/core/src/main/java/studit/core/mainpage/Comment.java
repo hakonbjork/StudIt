376091aec82e8546ff2f42f064577d52faf5215a
Erlend Hermanrud <erleherm@stud.ntnu.no>
2020-10-18 08:26:38 +0000

    Added new comment and discussion objects

---
+++ StudIt/core/src/main/java/studit/core/mainpage/Comment.java
@@ -0,0 +1,75 @@
+package studit.core.mainpage;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class Comment {
+  private String brukernavn;
+  private String kommentar;
+  private String dato;
+  private int upvotes;
+  private int downvotes;
+  private final int uniqueID;
+
+  public Comment(String brukernavn, String kommentar, int uniqueID) {
+    this.brukernavn = brukernavn;
+    this.kommentar = kommentar;
+    this.dato = new SimpleDateFormat("dd-MM-yyyy hh:mm:ss").format(Calendar.getInstance().getTime());
+    this.uniqueID = uniqueID;
+    this.upvotes = 0;
+    this.downvotes = 0;
+  }
+
+  public String getBrukernavn() {
+    return brukernavn;
+  }
+
+  public void setBrukernavn(String brukernavn) {
+    this.brukernavn = brukernavn;
+  }
+
+  public String getDato() {
+    return dato;
+  }
+
+  public void setDato(String dato) {
+    this.dato = dato;
+  }
+
+  public String getKommentar() {
+    return kommentar;
+  }
+
+  public void setKommentar(String kommentar) {
+    this.kommentar = kommentar;
+  }
+
+  public int getUpvotes() {
+    return upvotes;
+  }
+
+  public void setUpvotes(int upvotes) {
+    this.upvotes = upvotes;
+  }
+
+  public int getDownvotes() {
+    return downvotes;
+  }
+
+  public void setDownvotes(int downvotes) {
+    this.downvotes = downvotes;
+  }
+
+  public int getUniqueID() {
+    return uniqueID;
+  }
+
+  public void upvote() {
+    upvotes++;
+  }
+
+  public void downvote() {
+    downvotes++;
+  }
+
+}
---
+++ StudIt/core/src/main/java/studit/core/mainpage/Discussion.java
@@ -0,0 +1,64 @@
+package studit.core.mainpage;
+
+import java.util.Map;
+
+public class Discussion {
+
+  private Map<Integer, Comment> comments;
+  private int prevAssignedID = -1;
+
+  public Map<Integer, Comment> getComments() {
+    return comments;
+  }
+
+  public void setComments(Map<Integer, Comment> comments) {
+    this.comments = comments;
+  }
+
+  /**
+   * Adds a new comment to the discussion
+   * @param brukernavn unique username that sendt the comment
+   * @param kommentar written comment
+   * @param dato date written
+   */
+  public void addComment(String brukernavn, String kommentar) {
+    prevAssignedID += 1;
+    comments.put(prevAssignedID, new Comment(brukernavn, kommentar, prevAssignedID));
+  }
+
+  /**
+   * Upvotes a comment in the discussion
+   * @param ID Unique identifier for the comment (comments key)
+   */
+  public void upvote(int ID) {
+    comments.get(ID).upvote();
+  }
+
+  /**
+   * Downvotes a comment in the discussion
+   * @param ID Unique identifier for the comment (comments key)
+   */
+  public void downvote(int ID) {
+    comments.get(ID).downvote();
+  }
+
+  /**
+   * Get the requested Comment object
+   * @param ID Uniquie identifier for the comment (comments key)
+   * @return Comment object if ID is valid, otherwise null
+   */
+  public Comment getCommentByID(int ID) {
+    return comments.get(ID);
+  }
+
+  /**
+   * Removes a comment from the discussion (CANNOT BE UNDONE)
+   * @param ID Uniquie identifier for the comment (comments key)
+   * @return true if element was removed else false
+   */
+  public boolean removeComment(int ID) {
+    Comment comment = comments.remove(ID); 
+    return comment == null ? false : true; 
+  }
+
+}
