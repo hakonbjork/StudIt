4740c188a1c65205fb911cbd6717c18814dbfb53
Erlend Hermanrud <erleherm@stud.ntnu.no>
2020-10-26 16:12:52 +0000

    removed decrepated imports

--- StudIt/restserver/src/main/java/studit/restserver/DefaultGenerator.java
+++ StudIt/restserver/src/main/java/studit/restserver/DefaultGenerator.java
@@ -1,10 +1,8 @@
 package studit.restserver;
 
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
-import java.io.Reader;
 import java.io.Writer;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
