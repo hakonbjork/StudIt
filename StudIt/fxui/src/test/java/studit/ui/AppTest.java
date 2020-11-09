0444fa789d982b5e706cdfa72b49702a11bcbf70
Anita Stuberg Guldseth <anitasg@stud.ntnu.no>
2020-11-09 17:42:37 +0000

    added test init and currentUser to AppTest

--- StudIt/fxui/src/test/java/studit/ui/AppTest.java
+++ StudIt/fxui/src/test/java/studit/ui/AppTest.java
@@ -1,148 +1,142 @@
-// package studit.ui;
+package studit.ui;
 
-// import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-// import org.assertj.core.api.Assertions;
-// import org.junit.jupiter.api.Test;
-// import org.testfx.api.FxAssert;
-// import org.testfx.framework.junit5.ApplicationTest;
-// import org.testfx.matcher.base.WindowMatchers;
-// import javafx.fxml.FXMLLoader;
-// import javafx.scene.Parent;
-// import javafx.scene.Scene;
-// import javafx.stage.Stage;
+import java.util.List;
 
-// public class AppTest extends ApplicationTest {
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testfx.api.FxAssert;
+import org.testfx.framework.junit5.ApplicationTest;
+import org.testfx.matcher.base.WindowMatchers;
 
-//   private AppController appController;
-//   // private CourseController courseController;
-//   // private ObservableList<String> list = FXCollections.observableArrayList();
-//   // private ListView<String> listView;
-//   // String a = "TDT4109";
-//   // String b = "TMA4145";
-//   // String c = "TTM4175";
-//   // String d = "IT1901";
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import studit.core.mainpage.CourseItem;
+import studit.core.users.User;
+import studit.ui.remote.ApiCallException;
+import studit.ui.remote.DirectStuditModelAccess;
 
-//   @Override
-//   public void start(final Stage stage) throws Exception {
-//     final FXMLLoader appLoader = new FXMLLoader(getClass().getResource("App.fxml"));
-//     // final FXMLLoader courseLoader = new
-//     // FXMLLoader(getClass().getResource("App.fxml"));
+public class AppTest extends ApplicationTest {
 
-//     final Parent root = appLoader.load();
-//     this.appController = appLoader.getController();
-//     stage.setScene(new Scene(root));
-//     stage.show();
-//   }
+  private DirectStuditModelAccess directStuditModelAccess = new DirectStuditModelAccess();
+  private AppController appController = new AppController();
+  private ObservableList<CourseItem> list = FXCollections.observableArrayList();
+  private ListView<CourseItem> listView;
 
-//   // @BeforeEach
-//   // public void setup() throws Exception {
-//   // list.addAll(a, b, c, d);
-//   // listView.setItems(list);
-//   // }
+  @Override
+  public void start(final Stage stage) throws Exception {
+    final FXMLLoader appLoader = new FXMLLoader(getClass().getResource("App.fxml"));
 
-//   // @Test
-//   // public void hasChatBotButton() {
-//   // BorderPane rootNode = (BorderPane)
-//   // appController.rootPane.getScene().getRoot();
-//   // Button button = from(rootNode).lookup(".button").query();
-//   // assertEquals("Chatbot", button.getText());
-//   // }
+    final Parent root = appLoader.load();
+    this.appController = appLoader.getController();
+    stage.setScene(new Scene(root));
+    stage.show();
+  }
 
-//   @Test
-//   public void testAppController() {
-//     assertNotNull(this.appController);
-//   }
+  @Test
+  public void testInitialize() throws ApiCallException {
+    appController.initialize();
+  }
 
-//   @Test 
-//   public void testSetLabel(){
-//     String label = "TDT4109";
-//     appController.setLabel(label);
-//     Assertions.assertThat(appController.getLabel().equals(label));
-//   }
+  @Test
+  public void testCurrentUser(){
+    User currentUser = appController.getCurrentUser();
+    assertNotEquals(currentUser, null);
+  }
 
+  @BeforeEach
+  public void setup() throws ApiCallException {
+    list.addAll(directStuditModelAccess.getCourseList().getCourseItems());
+    listView.setItems(list);
+  }
 
-//   // @Test
-//   // public void testListView() {
-//   // assertNotNull(this.listView);
-//   // }
+  @Test
+  public void hasChatBotButton() {
+    BorderPane rootNode = (BorderPane) appController.rootPane.getScene().getRoot();
+    Button button = from(rootNode).lookup(".button").query();
+    assertEquals("Chatbot", button.getText());
+  }
 
-//   // @Test
-//   // public void hasMainPageButton() {
-//   // BorderPane rootNode = (BorderPane)
-//   // appController.rootPane.getScene().getRoot();
-//   // Button button = from(rootNode).lookup(".button").query();
-//   // assertEquals("Hjem", button.getText());
-//   // }
+  @Test
+  public void testAppController() {
+    assertNotNull(this.appController);
+  }
 
-//   // @Test
-//   // public void hasLogoutButton() {
-//   // BorderPane rootNode = (BorderPane)
-//   // appController.rootPane.getScene().getRoot();
-//   // Button button = from(rootNode).lookup("Logg ut").query();
-//   // assertEquals("Logg ut", button.getText());
-//   // }
+  @Test
+  public void testSetLabel() {
+    String label = "TDT4109";
+    appController.setLabel(label);
+    Assertions.assertThat(appController.getLabel().equals(label));
+  }
 
-//   // @Test
-//   // public void hasNtnuButton() {
-//   // BorderPane rootNode = (BorderPane)
-//   // appController.rootPane.getScene().getRoot();
-//   // Button button = from(rootNode).lookup("NTNU").query();
-//   // assertEquals("NTNU", button.getText());
-//   // }
+  @Test
+  public void testListView() {
+    assertNotNull(this.listView);
+  }
 
-//   @Test
-//   public void testLogoutAction() {
-//     clickOn("#logout_btn");
-//     FxAssert.verifyThat(window("Login"), WindowMatchers.isShowing());
-//   }
+  @Test
+  public void hasMainPageButton() {
+    BorderPane rootNode = (BorderPane) appController.rootPane.getScene().getRoot();
+    Button button = from(rootNode).lookup(".button").query();
+    assertEquals("Fagoversikt", button.getText());
+  }
 
-//   // @Test
-//   // public void testOpenChatBot() {
-//   // clickOn("#chatbot_btn");
-//   // FxAssert.verifyThat(window("Chatbot"), WindowMatchers.isShowing());
-//   // }
+  @Test
+  public void hasLogoutButton() {
+    BorderPane rootNode = (BorderPane) appController.rootPane.getScene().getRoot();
+    Button button = from(rootNode).lookup("Logg ut").query();
+    assertEquals("Logg ut", button.getText());
+  }
 
+  @Test
+  public void testLogoutAction() {
+    clickOn("#logout_btn");
+    FxAssert.verifyThat(window("Login"), WindowMatchers.isShowing());
+  }
 
-//   // @SuppressWarnings("unchecked")
-//   // @Test
-//   // public void testClickOnCourse() {
-//   // ListView<String> listView = (ListView<String>) lookup("#coursesList");
-//   // clickOn(listView.getItems().get(2));
-//   // Assertions.assertThat(listView.getSelectionModel().getSelectedItem().equals("IT1901"));
-//   // Assertions.assertThat(appController.mainScene.equals(course))
+  @Test
+  public void testOpenChatBot() {
+    clickOn("#chatbot_btn");
+    FxAssert.verifyThat(window("Chatbot"), WindowMatchers.isShowing());
+  }
 
-//   // FxAssert.verifyThat(window("Chatbot"), WindowMatchers.isShowing());
-//   // }
+  @SuppressWarnings("unchecked")
+  @Test
+  public void testClickOnCourse() {
+    ListView<String> listView = (ListView<String>) lookup("#coursesList");
+    clickOn(listView.getItems().get(2));
+    Assertions.assertThat(listView.getSelectionModel().getSelectedItem().equals("IT1901")); // hvilket fag skal være her
+    FxAssert.verifyThat(window("Course"), WindowMatchers.isShowing());
+  }
 
-//   // @SuppressWarnings("unchecked")
-//   // @Test
-//   // public void testClickOnCourse() {
-//   // ListView<String> listView = (ListView<String>) lookup("#coursesList");
-//   // clickOn(listView.getItems().get(2));
-//   // listView.getSelectionModel().getSelectedItem();
+  // @SuppressWarnings("#unchecked")
+  // @Test
+  // public void hasExactlyNumItems() {
+  //   ListView<CourseItem> listView = (ListView<CourseItem>lookup) ("#coursesList");
+  //   int numberOfItems = listView.getItems().size();
+  //   int actualNumber = 2;
+  //   assertEquals(numberOfItems, actualNumber);
+  // }
 
-//   // }
-
-//   // @SuppressWarnings("#unchecked")
-//   // @Test
-//   // public void hasExactlyNumItems() {
-//   // ListView<String> listView = (ListView<String>) lookup("#coursesList");
-//   // int numberOfItems = listView.getItems().size();
-//   // assertEquals(numberOfItems, 4);
-//   // }
-
-//   // // @Test
-//   // // public void comment() {
-//   // // String comment = "Jeg synes dette er et kjedelig fag";
-//   // // clickOn("#comment_btn");
-//   // // Assertions.verifyThat(appController.comment1.getText().equals("Jeg synes
-//   // // dette er et kjedelig fag"));
-//   // // }
-
-//   // @Test
-//   // public void testClickOnMainPage() {
-//   // Button button = (Button) lookup("#mainPage_btn");
-//   // clickOn(button);
-//   // }
-// }
+  @Test
+  public void testSearch() {
+    String name = "Informatikk prosjekt";
+    String code = "IT1901";
+    appController.searchField.setText("Informatikk");
+    CourseItem item2 = appController.coursesList.getItems().get(1);
+    assertEquals(item2.getFagnavn(), name);
+    assertEquals(item2.getFagkode(), code);
+  }
+}
